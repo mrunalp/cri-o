edb872bee42eb01ecc930075e25a4bda24fa1854
Mrunal Patel <mrunalp@gmail.com>
2018-03-12 12:03:54 -0700

    Implement container log reopen
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- server/container_reopen_log.go
+++ server/container_reopen_log.go
@@ -2,12 +2,55 @@ package server
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/kubernetes-incubator/cri-o/oci"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
+	"golang.org/x/sys/unix"
 	pb "k8s.io/kubernetes/pkg/kubelet/apis/cri/runtime/v1alpha2"
 )
 
 // ReopenContainerLog reopens the containers log file
-func (s *Server) ReopenContainerLog(ctx context.Context, in *pb.ReopenContainerLogRequest) (*pb.ReopenContainerLogResponse, error) {
-	return nil, fmt.Errorf("not yet implemented")
+func (s *Server) ReopenContainerLog(ctx context.Context, req *pb.ReopenContainerLogRequest) (resp *pb.ReopenContainerLogResponse, err error) {
+	const operation = "container_reopen_log"
+	defer func() {
+		recordOperation(operation, time.Now())
+		recordError(operation, err)
+	}()
+
+	logrus.Debugf("ReopenContainerLogRequest %+v", req)
+	containerID := req.ContainerId
+	c := s.GetContainer(containerID)
+
+	if c == nil {
+		return nil, fmt.Errorf("could not find container %q", containerID)
+	}
+
+	if err := s.ContainerServer.Runtime().UpdateStatus(c); err != nil {
+		return nil, err
+	}
+
+	cState := s.ContainerServer.Runtime().ContainerStatus(c)
+	if !(cState.Status == oci.ContainerStateRunning || cState.Status == oci.ContainerStateCreated) {
+		return nil, fmt.Errorf("container is not created or running")
+	}
+
+	controlPath := filepath.Join(c.BundlePath(), "ctl")
+	controlFile, err := os.OpenFile(controlPath, unix.O_WRONLY, 0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open container ctl file: %v", err)
+	}
+	defer controlFile.Close()
+
+	_, err = fmt.Fprintf(controlFile, "%d %d %d\n", 2, 0, 0)
+	if err != nil {
+		logrus.Infof("Failed to write to control file to reopen log file: %v", err)
+	}
+
+	resp = &pb.ReopenContainerLogResponse{}
+	logrus.Debugf("ReopenContainerLogResponse %s: %+v", containerID, resp)
+	return resp, nil
 }
