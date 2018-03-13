76126f5c7aa92f3f27ab737883496396e3ede52f
Mrunal Patel <mrunalp@gmail.com>
2018-03-13 14:11:32 -0700

    Add watch for container log reopen
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- server/container_reopen_log.go
+++ server/container_reopen_log.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/kubernetes-incubator/cri-o/oci"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
@@ -45,12 +46,53 @@ func (s *Server) ReopenContainerLog(ctx context.Context, req *pb.ReopenContainer
 	}
 	defer controlFile.Close()
 
-	_, err = fmt.Fprintf(controlFile, "%d %d %d\n", 2, 0, 0)
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
+		return nil, fmt.Errorf("Failed to create new watch: %v", err)
+	}
+	defer watcher.Close()
+
+	done := make(chan struct{})
+	errorCh := make(chan error)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				logrus.Debugf("event: %v", event)
+				if event.Op&fsnotify.Create == fsnotify.Create || event.Op&fsnotify.Write == fsnotify.Write {
+					logrus.Debugf("file created %s", event.Name)
+					if event.Name == c.LogPath() {
+						logrus.Debugf("expected log file created")
+						close(done)
+						return
+					}
+				}
+			case err := <-watcher.Errors:
+				errorCh <- fmt.Errorf("watch error for container log reopen %v: %v", containerID, err)
+				return
+			case <-s.monitorsChan:
+				errorCh <- fmt.Errorf("closing log reopen monitor for %v", containerID)
+				return
+			}
+		}
+	}()
+	cLogDir := filepath.Dir(c.LogPath())
+	if err := watcher.Add(cLogDir); err != nil {
+		logrus.Errorf("watcher.Add(%q) failed: %s", cLogDir, err)
+		close(done)
+	}
+
+	if _, err = fmt.Fprintf(controlFile, "%d %d %d\n", 2, 0, 0); err != nil {
 		logrus.Infof("Failed to write to control file to reopen log file: %v", err)
 	}
 
-	resp = &pb.ReopenContainerLogResponse{}
+	select {
+	case err := <-errorCh:
+		return nil, err
+	case <-done:
+		resp = &pb.ReopenContainerLogResponse{}
+	}
+
 	logrus.Debugf("ReopenContainerLogResponse %s: %+v", containerID, resp)
 	return resp, nil
 }
